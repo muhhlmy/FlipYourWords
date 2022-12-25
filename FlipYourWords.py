944fbaff86d0eb05c81ce75f8d7d6f2e6ae47cc2
muhhlmy <120382858+muhhlmy@users.noreply.github.com>
2022-12-25 14:08:42 +0700

    Update FlipYourWords.py

--- FlipYourWords.py
+++ FlipYourWords.py
@@ -1,12 +1,6 @@
-import os
 import streamlit as st
 
-#Fungsi
-def cls():
-    os.system('cls')
-
 #Program Membalikkan Kata
-cls()
 st.title('Flip your words!')
 String = st.text_input('Type a Word') [::-1]
 
